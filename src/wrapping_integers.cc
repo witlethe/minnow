ef568cba628ba6ad5d9fccd865755ecd35e754cf
witlethe <jzsteven614@gmail.com>
2026-03-27 20:26:32 +0800

    complete Wrap32

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -3,16 +3,32 @@
 
 using namespace std;
 
+namespace {
+  uint64_t abs_diff(const uint64_t& a, const uint64_t& b) {
+    if (a > b) {
+      return a - b;
+    }
+    return b - a;
+  }
+} // namespace
+
+/* convert absolute seqno to seqno */
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  debug( "unimplemented wrap( {}, {} ) called", n, zero_point.raw_value_ );
-  return Wrap32 { 0 };
+  return Wrap32 { zero_point.raw_value_ + static_cast<uint32_t>( n ) };
 }
 
+/* convert seqno to cloest absolute seqno */
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  debug( "unimplemented unwrap( {}, {} ) called", zero_point.raw_value_, checkpoint );
-  return {};
+  auto z = zero_point.raw_value_;
+  uint64_t offset = (raw_value_ - z) % (1ULL<<32);    // 我大概一辈子不会忘记你了，左移运算符优先级 :-)
+  uint64_t anchor_base = (checkpoint & 0xFFFFFFFF00000000) | offset;
+  uint64_t anchor_larger = anchor_base + (1ULL<<32);
+  uint64_t anchor_smaller = anchor_base - (1ULL<<32);
+
+  if (anchor_base > checkpoint) {
+    return abs_diff(anchor_base, checkpoint) < abs_diff(anchor_smaller, checkpoint) ? anchor_base : anchor_smaller;
+  }      
+  return abs_diff(anchor_base, checkpoint) < abs_diff(anchor_larger, checkpoint) ? anchor_base : anchor_larger;
 }
